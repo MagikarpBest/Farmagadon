2d8dc18790312a97bc587128d5e1dd1825c890cf
yTan <yoru1781@gmail.com>
2025-10-10 01:02:33 +0800

    eexuan face

--- Assets/Scripts/ButtonEvent/LoadOutManager.cs
+++ Assets/Scripts/ButtonEvent/LoadOutManager.cs
@@ -1,21 +1,37 @@
-//using UnityEngine;
-//using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+public class LoadOutManager : MonoBehaviour
+{
+    [SerializeField] private Transform loadoutPanel;
+    [SerializeField] private GameObject loadoutSlotPrefab;
 
-//public class LoadOutManager : MonoBehaviour
-//{
-//    [SerializeField] private Transform loadoutPanel;
-//    [SerializeField] private GameObject loadoutSlotPrefab;
+    private List<GameObject> currentLoadout = new List<GameObject>();
 
-//    //private List<GameObject> currentLoadout = new List<GameObject>();
+    public void AddToLoadout(string itemName)
+    {
+        // Optional: limit number of loadout items (e.g., 4)
+        if (currentLoadout.Count >= 4)
+        {
+            Debug.Log("Loadout full!");
+            return;
+        }
 
+        // Create new slot button
+        GameObject slot = Instantiate(loadoutSlotPrefab, loadoutPanel);
+        slot.GetComponentInChildren<TextMeshProUGUI>().text = itemName;
 
-//    private void AddToLoadout(string weaponName)
-//    {
-//        if (currentLoadout.Count > 4)
-//        {
-//            Debug.Log("Full");
-//            return;
-//        }
-//    }
+        // Add remove functionality
+        Button btn = slot.GetComponent<Button>();
+        btn.onClick.AddListener(() => RemoveFromLoadout(slot));
 
-//}
+        currentLoadout.Add(slot);
+    }
+
+    public void RemoveFromLoadout(GameObject slot)
+    {
+        currentLoadout.Remove(slot);
+        Destroy(slot);
+    }
+}
