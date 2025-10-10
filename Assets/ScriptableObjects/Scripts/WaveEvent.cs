aeb2d93f44414e42bbc199f3ec13c04ed551553e
MagikarpBest <MagikarpBest@users.noreply.github.com>
2025-10-11 00:04:50 +0800

    teterer
    rerer

--- Assets/ScriptableObjects/Scripts/WaveEvent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "WaveSystem", fileName = "NewWaveEvent")] 
-public class WaveEvent : ScriptableObject
-{
-    [Header("Timing")]
-    public float time;               // When this happen
-    public EnemySpawnInfo[] enemies; // What to spawn
-}
