0e613f41a23e722c768a7400236e8cd4691ee98d
yTan <yoru1781@gmail.com>
2025-10-23 00:14:06 +0800

    loadout keyboard input

--- Assets/Scripts/ButtonEvent/LoadOutManager.cs
+++ Assets/Scripts/ButtonEvent/LoadOutManager.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.InputSystem;
+using System.Xml.Serialization;
 
 public class LoadOutManager : MonoBehaviour
 {
+    [SerializeField] private GameInput gameInput;
     [SerializeField] private List<Button> loadoutSlots;
     [SerializeField] private GameObject loadoutSlotPrefab;
 
@@ -14,9 +17,88 @@ public class LoadOutManager : MonoBehaviour
     Dictionary<int, GameObject> slotItems = new Dictionary<int, GameObject>();
     private List<GameObject> currentLoadout = new List<GameObject>();
 
+    private float moveCooldown = 0.2f;
+    private float lastMoveTime = 0f;
 
-    //private Color normalColor = Color.white;
-    //private Color selectedColor = Color.yellow;
+    private void Start()
+    {
+        //highlight first slot
+        HighlightSlot(selectedIndex, true);
+    }
+
+    private void OnEnable()
+    {
+        if(gameInput != null)
+        {
+            gameInput.OnShootAction += ComfirmSelection;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (gameInput != null)
+        {
+            gameInput.OnShootAction -= ComfirmSelection;
+        }
+    }
+
+    private void Update()
+    {
+        HandleKeyboardNavigation();
+    }
+
+    private void HandleKeyboardNavigation()
+    {
+        if (gameInput == null) return;
+
+        Vector2 moveInput = gameInput.GetMovementVectorNormalized();
+
+        if (Mathf.Abs(moveInput.x) > 0.5f && Time.time - lastMoveTime > moveCooldown)
+        {
+            if (moveInput.x > 0)
+                MoveSelection(1); // D / Right
+            else
+                MoveSelection(-1); // A / Left
+
+            lastMoveTime = Time.time;
+        }
+    }
+
+    private void MoveSelection(int direction)
+    {
+        if (loadoutSlots.Count == 0) return;
+
+        // Deselect current
+        HighlightSlot(selectedIndex, false);
+
+        // Move index
+        selectedIndex += direction;
+        if (selectedIndex < 0) selectedIndex = loadoutSlots.Count - 1;
+        else if (selectedIndex >= loadoutSlots.Count) selectedIndex = 0;
+
+        // Select new
+        HighlightSlot(selectedIndex, true);
+        slotActive = true;
+
+        Debug.Log($"Selected slot: {selectedIndex}");
+    }
+
+    private void ComfirmSelection()
+    {
+        if(!slotActive) return;
+
+        Debug.Log("Added");
+        AddToLoadout("Test");
+    }
+
+    private void HighlightSlot(int index, bool highlight)
+    {
+        if (index < 0 || index >= loadoutSlots.Count) return;
+        Image img = loadoutSlots[index].GetComponent<Image>();
+
+        if (img != null)
+            img.color = highlight ? Color.yellow : Color.white;
+    }
 
     public void AddToLoadout(string itemName)
     {
@@ -27,7 +109,7 @@ public class LoadOutManager : MonoBehaviour
             return;
         }
 
-        int index=selectedIndex;
+        int index = selectedIndex;
 
         // Prevent double assignment
         if (slotItems.ContainsKey(index))
@@ -47,6 +129,7 @@ public class LoadOutManager : MonoBehaviour
 
         // Create new slot button
         //var slot = loadoutSlots;
+        Debug.Log("created");
         GameObject icon = Instantiate(loadoutSlotPrefab, slotTransform);
 
         TextMeshProUGUI text = icon.GetComponentInChildren<TextMeshProUGUI>(true);
