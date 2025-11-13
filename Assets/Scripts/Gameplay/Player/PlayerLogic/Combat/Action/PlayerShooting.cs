e2b1f5b2af8edb53470a39b5502f9f19b973f3dd
MagikarpBest <MagikarpBest@users.noreply.github.com>
2025-11-13 23:24:52 +0800

    reward card change

--- Assets/Scripts/Gameplay/Player/PlayerLogic/Combat/Action/PlayerShooting.cs
+++ Assets/Scripts/Gameplay/Player/PlayerLogic/Combat/Action/PlayerShooting.cs
@@ -115,7 +115,6 @@ public class PlayerShooting : MonoBehaviour
 
         // Wait for the weapon's fire rate duration
         yield return visualAnimation;
-        //yield return new WaitForSeconds(weapon.fireRate);
 
         if (playerMovement != null)
         {
